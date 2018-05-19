acb6b8fd146fcfa2526eddf95b88a4e3e5bb5ad7
David García Garzón <voki@canvoki.net>
2018-05-19 15:27:06 +0200

    test wo installing requires absolute import

--- vitae/__main__.py
+++ vitae/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from . import Vitae, Position, Education, Publication, Work, Course, Award
+from vitae import Vitae, Position, Education, Publication, Work, Course, Award
 from yamlns import namespace as ns
 import os, codecs, sys
 
